c13d97031edc56ab8678a94760382c0ca96c68aa
milkyway90 <ignacymilocha4@gmail.com>
2021-11-20 21:16:40 +0100

    made a calculator

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-import 'bootstrap@4.6.0'
-import $ from 'jquery'
+const objetosc = {
+    objetosc(){
+        alert('objetosc to: ' + 2 * a * b + 2 * a * c + 2 * b * c)
+    }
+};
 
-$('button')
-  .html('Click me') // Try edit it...
-  .on('click', () => alert('button clicked'));
+const pole = {
+    pole(a, b, c){
+        alert('pole to: ' + a * b * c);
+    }
+};
 
+var choose = window.prompt('wybierz: \n pole - 1 \n objetosc - 0 \n twoj wybor: ');
+var a = window.prompt('liczba a: ');
+var b = window.prompt('liczba b: ');
+var c = window.prompt('liczba c: ');
+
+if(choose == '1'){
+    pole.pole(a, b, c);
+}else if(choose == '1'){
+    objetosc.objetosc(a, b, c);
+}else{
+    alert('something wrong m8');
+}
